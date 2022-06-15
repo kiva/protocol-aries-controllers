1d2a90137790e9b6735a62d988210f7549b18649
Jeff Kennedy <jeffk@kiva.org>
2022-06-15 21:10:06 +0100

    prod_setup script needs to update reference to script location in prod containers.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/prod_setup.sh
+++ scripts/prod_setup.sh
@@ -2,8 +2,8 @@
 # Simple setup of each controller to be run as part of the integration tests
 set -ev
 
-docker exec -it kiva-controller node /www/scripts/setup.employee.kiva.js
-docker exec -it kiva-controller node /www/scripts/setup.note.kiva.js
-docker exec -it tdc-controller node /www/scripts/setup.tdc.js
-docker exec -it tdc-controller node /www/scripts/setup.citizen.tdc.js
+docker exec -it kiva-controller node /www/dist/scripts/setup.employee.kiva.js
+docker exec -it kiva-controller node /www/dist/scripts/setup.note.kiva.js
+docker exec -it tdc-controller node /www/dist/scripts/setup.tdc.js
+docker exec -it tdc-controller node /www/dist/scripts/setup.citizen.tdc.js
 
